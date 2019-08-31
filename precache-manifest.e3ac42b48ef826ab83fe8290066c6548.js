ea0c0622b79e007fc4a2b7e9670db8aa3d0137dd
Andreas <andreas.larsson@dotnetmentor.se>
2019-09-01 00:14:31 +0200

    Updates

---
+++ precache-manifest.e3ac42b48ef826ab83fe8290066c6548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7c0f2bad841f2c181e8be0412210ef",
+    "url": "/next-departure/index.html"
+  },
+  {
+    "revision": "de74cb8bcea1c8471b70",
+    "url": "/next-departure/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6ec3111ecf5fcd0f9825",
+    "url": "/next-departure/static/js/2.54a6459b.chunk.js"
+  },
+  {
+    "revision": "de74cb8bcea1c8471b70",
+    "url": "/next-departure/static/js/main.60d3fae9.chunk.js"
+  },
+  {
+    "revision": "e858da17612f660c67d2",
+    "url": "/next-departure/static/js/runtime~main.04045d02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/next-departure/static/media/logo.5d5d9eef.svg"
+  }
+]);
