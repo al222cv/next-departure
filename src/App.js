739c212d2197a0de7226c51a43ae11995e4bbd6b
Andreas <andreas.larsson@dotnetmentor.se>
2019-09-01 00:13:09 +0200

    added deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
+  const [departures, setDeparures] = useState([])
+
+  useEffect(() => makeCall(), [])
+
+  const makeCall = () => {
+    fetch(
+      'https://api.resrobot.se/v2/departureBoard?key=4e22257c-71be-4b0b-b3dc-7ad652b0d8f6&id=740025626&maxJourneys=10&format=json'
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json.Departure)
+        setDeparures(json.Departure)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  useInterval(() => makeCall(), 10000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <table>
+        <thead>
+          <tr>
+            <th>Linje</th>
+            <th>Riktning</th>
+            <th>Nästa avgång</th>
+          </tr>
+        </thead>
+        <tbody>
+          {departures.map(dep => (
+            <tr>
+              <td>{dep.transportNumber}</td>
+              <td>{dep.direction}</td>
+              <td>{dep.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div />
     </div>
-  );
+  )
 }
 
-export default App;
+function useInterval (callback, delay) {
+  const savedCallback = useRef()
+
+  // Remember the latest callback.
+  useEffect(
+    () => {
+      savedCallback.current = callback
+    },
+    [callback]
+  )
+
+  // Set up the interval.
+  useEffect(
+    () => {
+      function tick () {
+        savedCallback.current()
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay)
+        return () => clearInterval(id)
+      }
+    },
+    [delay]
+  )
+}
+
+export default App
